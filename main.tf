a029e567b94bdc682ab53d21d32ad1323b0e3a4c
Pooja Agrawal <poojaagr2193@gmail.com>
2025-07-22 23:05:31 +0000

    Cleanup: Remove duplicate Terraform files from repo root

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "my_ec2_instance" {
-	ami = "ami-05dc67d613ea82750"
-	instance_type = "t3.micro"
-
-	tags = {
-		Name = "MyFirstEC2"
-	}
-}
-
--- output.tf
+++
@@ -1,6 +0,0 @@
-output "instance_public_ip" {
-	description = "The public IP address of the EC2 instance"
-	value = aws_instance.my_ec2_instance.public_ip
-}
-
-
